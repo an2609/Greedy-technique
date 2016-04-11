df9d686cdab8a5581e849790aad4499ca92d52c5
Ankur Singla <ankursingla260@gmail.com>
2016-04-11 19:32:16 +0530

    Update Dijkstra O(n^2).cpp

--- Dijkstra O(n^2).cpp
+++ Dijkstra O(n^2).cpp
@@ -1,59 +1,84 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 1000
 #define INF INT_MAX
+#define MAX 1000
 int dist[MAX];
 int spt[MAX];
 int find_min(int spt[],int dist[],int n)
 {
-    int mn = INF;
-    int in;
+    int mn=INF,in;
     for(int i=1; i<=n; i++)
     {
-        if (!spt[i] && dist[i]<mn)
+        if(!spt[i]&&dist[i]<mn)
         {
-            mn = dist[i];
-            in = i;
+            mn=dist[i];
+            in=i;
         }
     }
-    if (mn==INF) return -1;
+    if(mn==INF)
+        return -1;
     return in;
 }
 int main()
 {
     int n,m,a,b,c,s;
+    char status;
     cout<<"Enter the number of nodes in the graph\n";
     cin>>n;
     int ar[n+1][n+1];
     memset(ar,0,sizeof(ar));
     cout<<"Enter the number of edges\n";
     cin>>m;
-    cout<<"Enter the edges in form of node a,node b,weight of edge\n";
-    while(m--)
+    cout<<"Is the graph directed?\n";
+    cin>>status;
+    if(status=='Y'||status=='y')
     {
-        scanf("%d%d%d",&a,&b,&c);
-        ar[a][b]=c;
+        cout<<"Enter the edges in form of node a,node b,weight of edge\n";
+        while(m--)
+        {
+            scanf("%d%d%d",&a,&b,&c);
+            ar[a][b]=c;
+        }
+    }
+    else
+    {
+        cout<<"Enter the edges in form of node a,node b,weight of edge\n";
+        while(m--)
+        {
+            scanf("%d%d%d",&a,&b,&c);
+            ar[a][b]=c;
+            ar[b][a]=c;
+        }
     }
     cout<<"Enter the source node\n";
     cin>>s;
-    for  (int i=0; i<=n; i++)
-        dist[i] = INF;
-    dist[s] = 0;
+    memset(spt,0,sizeof(spt));
+    for(int i=1; i<=n; i++)
+    {
+        dist[i]=INF;
+    }
+    dist[s]=0;
     while(1)
     {
-        int in = find_min(spt,dist,n);
-        spt[in] = 1;
-        for (int i=1; i<=n; i++)
+        int temp=find_min(spt,dist,n);
+        if(temp==-1)
+            break;
+        spt[temp]=1;
+        for(int i=1;i<=n;i++)
         {
-            if (ar[in][i]!=0 && !spt[i])
+            if(!spt[i]&&ar[temp][i]!=0)
             {
-                dist[i] = min(dist[i],dist[in]+ar[in][i]);
+                dist[i]=min(dist[i],dist[temp]+ar[temp][i]);
             }
         }
-        if (in==-1) break;
     }
     for(int i=1; i<=n; i++)
-        cout<<dist[i]<<" ";
-    cout<<endl;
-    return 0;
+    {
+        if(i==s)
+            continue;
+        else
+        {
+            printf("Shortest distance from %d to %d = %d\n",s,i,dist[i]);
+        }
+    }
 }
